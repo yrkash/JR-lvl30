965ddd144f051bfa2de50607646acc3c877aaaaa
yrkash <yrkash@gmail.com>
2022-11-01 13:09:25 +0300

    Added some methods to Model.java

--- src/com/javarush/task/task35/task3513/Model.java
+++ src/com/javarush/task/task35/task3513/Model.java
@@ -1,4 +1,45 @@
 package com.javarush.task.task35.task3513;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Model {
+
+    private static final int FIELD_WIDTH = 4;
+    private Tile[][] gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+    ;
+
+    public Model() {
+        resetGameTiles();
+    }
+
+    private List<Tile> getEmptyTiles() {
+        List<Tile> emptyTiles = Arrays.stream(gameTiles)
+                .flatMap(Arrays::stream)
+                .filter(Tile::isEmpty)
+                .collect(Collectors.toList());
+        return emptyTiles;
+    }
+
+    public void addTile() {
+        List<Tile> emptyTiles = this.getEmptyTiles();
+        if (emptyTiles.isEmpty()) return;
+        int randomTileNumber = (int) (Math.random() * emptyTiles.size());
+        int randomTileValue = (Math.random() < 0.9) ? 2 : 4;
+        Tile randomTile = emptyTiles.get(randomTileNumber);
+        randomTile.value = randomTileValue;
+    }
+
+    public void resetGameTiles() {
+
+        for (int i = 0; i < FIELD_WIDTH; i++) {
+            for (int j = 0; j < FIELD_WIDTH; j++) {
+                gameTiles[i][j] = new Tile();
+            }
+        }
+        addTile();
+        addTile();
+    }
 }
