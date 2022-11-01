fae13289252f7350a533e77529f23ae08e5dabe0
yrkash <yrkash@gmail.com>
2022-11-01 18:15:30 +0300

    Added compressTiles, mergeTiles methods to Model.java

--- src/com/javarush/task/task35/task3513/Model.java
+++ src/com/javarush/task/task35/task3513/Model.java
@@ -8,11 +8,46 @@ import java.util.stream.Collectors;
 public class Model {
 
     private static final int FIELD_WIDTH = 4;
-    private Tile[][] gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
-    ;
+    public Tile[][] gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+
+    int score;
+    int maxTile;
 
     public Model() {
         resetGameTiles();
+    }
+
+    private void compressTiles(Tile[] tiles) {
+        List<Tile> emptyList = new ArrayList<>();
+        List<Tile> numberList = new ArrayList<>();
+        for (int i = 0; i < tiles.length; i++) {
+            if (tiles[i].value == 0) {
+                emptyList.add(tiles[i]);
+            }
+            else {
+                numberList.add(tiles[i]);
+            }
+        }
+        for (int i = 0; i < numberList.size(); i++) {
+            tiles[i] = numberList.get(i);
+        }
+        for (int i = 0; i < emptyList.size(); i++) {
+            tiles[i + numberList.size()] = emptyList.get(i);
+        }
+    }
+
+    private void mergeTiles(Tile[] tiles) {
+        for (int i = 0; i < tiles.length - 1; i++) {
+            if (tiles[i].value == tiles[i + 1].value && tiles[i].value != 0) {
+                int updatedValue = tiles[i].value * 2;
+
+                if (maxTile < updatedValue) maxTile = updatedValue;
+                score += updatedValue;
+                tiles[i].value *= 2;
+                tiles[i + 1].value = 0;
+            }
+        }
+        compressTiles(tiles);
     }
 
     private List<Tile> getEmptyTiles() {
